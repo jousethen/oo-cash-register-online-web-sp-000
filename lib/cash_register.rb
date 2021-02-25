72846586183acba5e5f2300a6b17db397cdb2e31
Jouse then <jthen93@gmail.com>
2021-02-25 21:42:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
-
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :items
+  
+  def initialize(discount=0)
+    @total = 0.0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title, price, quantity=1)
+    quantity.times do
+      @items << title
+      @total += price
+    end
+  end
+  
+  def apply_discount
+    if discount > 0
+    
+      discount = @discount.to_f / 100.00
+      # binding.pry
+      @total = @total - (@total * discount)
+      
+      return "After the discount, the total comes to $#{@total.to_i}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+end
